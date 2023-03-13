87ded67e80f2133db3888cf63f408ec1d8870937
gleidsonteixeira <gleidsondeveloper@gmail.com>
2023-03-13 09:59:36 -0300

    salvando no localstorage

--- scripts.js
+++ scripts.js
@@ -1,43 +1,51 @@
-let alunos = [
-    {
-        nome: "gleidson",
-        email: "gleidson@gmail.com",
-        telefone: "(00) 12345-6789",
-        cidade: "Fortaleza" 
-    },
-    {
-        nome: "Bruno caucaia",
-        email: "brunão@gmail.com",
-        telefone: "(00) 12345-6789",
-        cidade: "Caucaia"
-    },
-    {
-        nome: "Kevin",
-        email: "kevin@gmail.com",
-        telefone: "(00) 12345-6789",
-        cidade: "Fortaleza"
-    },
-    {
-        nome: "Sarah",
-        email: "sarah@gmail.com",
-        telefone: "(00) 12345-6789",
-        cidade: "Fortaleza"
-    }
-];
+// let alunos = [
+//     {
+//         nome: "gleidson",
+//         email: "gleidson@gmail.com",
+//         telefone: "(00) 12345-6789",
+//         cidade: "Fortaleza" 
+//     },
+//     {
+//         nome: "Bruno caucaia",
+//         email: "brunão@gmail.com",
+//         telefone: "(00) 12345-6789",
+//         cidade: "Caucaia"
+//     },
+//     {
+//         nome: "Kevin",
+//         email: "kevin@gmail.com",
+//         telefone: "(00) 12345-6789",
+//         cidade: "Fortaleza"
+//     },
+//     {
+//         nome: "Sarah",
+//         email: "sarah@gmail.com",
+//         telefone: "(00) 12345-6789",
+//         cidade: "Fortaleza"
+//     }
+// ];
 
 function listarAluno(){
     let tabela = document.querySelector("#tabela");
     tabela.innerHTML = "";
     
-    for(let i = 0; i < alunos.length; i++){
+    let lista = JSON.parse(localStorage.getItem("lista")) || [];
+
+    for(let i = 0; i < lista.length; i++){
         tabela.innerHTML += `<tr>
-                                <td>${alunos[i].nome}</td>
-                                <td>${alunos[i].email}</td>
-                                <td>${alunos[i].telefone}</td>
-                                <td>${alunos[i].cidade}</td>
+                                <td>${lista[i].nome}</td>
+                                <td>${lista[i].email}</td>
+                                <td>${lista[i].telefone}</td>
+                                <td>${lista[i].cidade}</td>
                                 <td>
-                                    <button class="btn btn-warning">Editar</button>
-                                    <button class="btn btn-danger">Deletar</button>
+                                    <button 
+                                        class="btn btn-warning"
+                                        type="button" 
+                                        data-bs-toggle="offcanvas" 
+                                        data-bs-target="#offcanvasRightEditar" 
+                                        aria-controls="offcanvasRightEditar"
+                                        onclick="preencherForm(${i})">Editar</button>
+                                    <button class="btn btn-danger" onclick="deletarAluno(${i})">Deletar</button>
                                 </td>
                             </tr>`
     }
@@ -46,12 +54,57 @@ function listarAluno(){
 
 function adicionarAluno(){
     event.preventDefault();
+    //PEGAR A LISTA DE ALUNOS
+    let lista = JSON.parse(localStorage.getItem("lista")) || [];
+
+    //MONTA O OBJETO ALUNO
     let aluno = {
         nome: nome.value,
         email: email.value,
         telefone: telefone.value,
         cidade: cidade.value
     }
-    alunos.push(aluno);
+    //INSERE O ALUNO NA LISTA
+    lista.push(aluno);
+    
+    //SALVANDO NO LOCALSTORAGE
+    localStorage.setItem("lista", JSON.stringify(lista));
+    
+    //LIMPANDO OS CAMPOS DO FORMULARIO
+    cadastro.reset();
+
+    //FECHAR O MODAL
+    let fechar = document.querySelector("#cadastro-fechar");
+    fechar.dispatchEvent(new Event("click"));
+
+    //LISTANDO ALUNOS NOVAMENTE
+    listarAluno();
+}
+
+function preencherForm(posicao)
+{
+    let lista = JSON.parse(localStorage.getItem("lista")) || [];
+    e_nome.value = lista[posicao].nome;
+    e_email.value = lista[posicao].email;
+    e_telefone.value = lista[posicao].telefone;
+    e_cidade.value = lista[posicao].cidade;
+}
+
+function editarAluno()
+{
+
+    
+}
+
+function deletarAluno(posicao)
+{
+    let lista = JSON.parse(localStorage.getItem("lista")) || [];
+    let novaLista = [];
+    for(let i = 0; i < lista.length; i++){
+        if(i != posicao){
+            novaLista.push(lista[i]);
+        }
+    }
+    localStorage.setItem("lista", JSON.stringify(novaLista))
     listarAluno();
 }
